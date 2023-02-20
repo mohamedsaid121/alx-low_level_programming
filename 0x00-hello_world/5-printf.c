3978626c64f05a85d93b378347dd50d342baee3e
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-20 20:39:50 +0000

    adjust brackets

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * Main - first point
- * the function always return 0
+ * Return : always 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return 0;
+		return (0);
 }
