004a4f838d12d9dc08b1ff92b1b515ae493fc7e6
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-20 20:36:07 +0000

    Add comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * Main - first point
+ * the function always return 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return 0;
 }
