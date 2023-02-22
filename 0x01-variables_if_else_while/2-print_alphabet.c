1b34e2b2f9b75cdb2d173c0db6145168bf35eaee
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-22 20:17:03 +0000

    print small alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m = 'a';
+
+	while (m <= 'z')
+	{
+		putchar(m);
+		m++;
+	}
+	return (0);
+}
