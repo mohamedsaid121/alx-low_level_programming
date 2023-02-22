da47f1f3ad17107091f96dedda68022652fae1be
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-22 21:15:05 +0000

    how to use continue

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar(a);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return (0);
+}
