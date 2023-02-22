056d83a973e2cc76299c462925e6ddb60e894a1e
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-22 19:42:56 +0000

    Modify last case

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 		{
 			printf("Last digit of n is %d and is 0\n", r);
 		}
-		else
+		else if (r < 6 && r != 0)
 		{
 			printf("Last digit of n is %d and is less than 6 and not 0\n", r);
 		}
