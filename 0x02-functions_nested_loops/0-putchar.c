f2c7e1478af764c6c0ef0b70677fce951738afb4
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-26 20:27:47 +0000

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function return _putchar word
+ * it's allowed to use putchar function only
+ * Return: always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This repo is about function and nested loops
