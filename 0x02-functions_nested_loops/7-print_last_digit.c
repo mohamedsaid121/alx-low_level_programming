4e6ff4086c8e8f929829f63aa21fc10a253aaf84
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-26 22:52:40 +0000

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - function to print last digit
+ * Return: last digit
+ * @n: argument of type int
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
