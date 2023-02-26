b76a25fca98be9df06a082775db3e29578c41f45
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-26 22:43:49 +0000

    abs number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function to print absolute value of number
+ * Return: 0
+ * @int: type of argument
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
