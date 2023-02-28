5067122c679ce2e3707660cbb32021ca4c0d648b
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-28 22:58:54 +0000

    add space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,20 +15,22 @@ int main(void)
 	{
 		if ((i % 3) == 0 && (i % 5) != 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((i % 5) == 0 && (i % 3) != 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((i % 3) != 0 && (i % 5) != 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
+		if (i < 100)
+			putchar(32);
 	}
 	putchar('\n');
 	return (0);
