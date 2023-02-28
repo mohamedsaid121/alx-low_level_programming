a3be04e90d352078141c3440472dbeb68c1fc224
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-28 20:00:42 +0000

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - Function that print diagonal using char \
+ * it's allowed only to use _putchar
+ * @n: argument of type int
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n == 0 || n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\');
+			_putchar('\n');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
