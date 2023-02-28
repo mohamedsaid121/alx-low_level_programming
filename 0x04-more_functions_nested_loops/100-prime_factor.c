362c53de12a1b366ede963c4042fd5f1094ea1d6
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-28 23:31:45 +0000

    prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function that gets the largest prime factor of number 612852475143
+ * Return:0
+ */
+int main(void)
+{
+	unsigned long int num = 612852475143;
+	unsigned long int prim;
+
+	prim = 3;
+	while (prim < num / 2)
+	{
+		if ((num % prim) == 0)
+		{
+			if ((prim % 3) == 2)
+			{
+				printf(",%lu ", prim);
+			}
+		}
+		prim += 2;
+	}
+	putchar('\n');
+	return (0);
+}
