548efbdc9d7a697c34ca602ecadd11737261fa4d
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-28 22:56:06 +0000

    remove space between fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * fizz_buzz - function that print numbers from 1 to 100 with certain condition
+ * main - function that print numbers from 1 to 100 with certain condition
  * condition 1:For multiples of three print Fizz instead of the number
  * condition 2:For the multiples of five print Buzz
  * condition 3:For numbers which are multiples of both 3 and 5 print FizzBuzz.
@@ -23,7 +23,7 @@ int main(void)
 		}
 		else if ((i % 3) != 0 && (i % 5) != 0)
 		{
-			printf("Fizz Buzz ");
+			printf("FizzBuzz ");
 		}
 		else
 		{
