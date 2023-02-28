a650a50e6b7af14128d03001e932d0118e20ac36
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-02-28 22:35:59 +0000

    change conditions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,6 +5,7 @@
  * condition 1:For multiples of three print Fizz instead of the number
  * condition 2:For the multiples of five print Buzz
  * condition 3:For numbers which are multiples of both 3 and 5 print FizzBuzz.
+ * Return: 0
  */
 int fizz_buzz(void)
 {
@@ -12,11 +13,11 @@ int fizz_buzz(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3) == 0 && (i % 5) != 0)
 		{
 			printf("Fizz ");
 		}
-		else if ((i % 5) == 0)
+		else if ((i % 5) == 0 && (i % 3) != 0)
 		{
 			printf("Buzz ");
 		}
