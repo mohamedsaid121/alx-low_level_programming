acc7ec04ec1eecdc1cdb55423aba393f2443ed98
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-03-03 15:37:32 +0000

    print in rev order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - function to print string in reverse order
+ * @s: argument pointer and char value
+ */
+void print_rev(char *s)
+{
+	count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	while (count--)
+	{
+		_putchar(*(s + count));
+	}
+	_putchar('\n')
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -23,5 +23,11 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 #endif
 
