2238740a161d2a7c5de3b710f1af245a85a5b416
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-03-03 00:42:36 +0000

    first pointer task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - function to update value using pointer
+ * @n: variable pointer and points to int value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory for basic pointer tasks
