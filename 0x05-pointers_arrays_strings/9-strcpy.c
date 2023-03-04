18df01efac48513dcaafb7bffad87ceae483d3e9
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-03-04 02:20:33 +0000

    modifying

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,10 @@ char  *_strcpy(char *dest, char *src)
 {
 	int count = 0;
 
-	while (*src != '\0')
+	while (*(src + count) != '\0')
 	{
-		dest[count] = *src;
+		*(dest + count) = *(src + count);
 		count++;
-		src++;
 	}
 	return (dest);
 }
