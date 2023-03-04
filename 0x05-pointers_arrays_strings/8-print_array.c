3fb13ebeefc384780f46d9dc0c9568240697da2e
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-03-04 01:43:06 +0000

    print element of arr

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  print_array - function print elements of arr
+ *  @a: argument pointer points to int value
+ *  @n: second argument of type int
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
