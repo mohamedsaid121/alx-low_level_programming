c6fc18e7c7e290c93ddbe16b3556b773d86abe39
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-03-04 01:51:21 +0000

    add if cond

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,8 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		_putchar(',');
-		_putchar(' ');
+		if ( i < n - 1)
+			_putchar(', ');
 	}
 	_putchar('\n');
 }
