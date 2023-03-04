54b2dc641e22f213db7a8d233ce649799a547786
mohamedsaid121 <mohameddmidoo522@gmail.com>
2023-03-04 02:14:30 +0000

    copy value form loc to another loc

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  *_strcpy - function that copy str from location to another location
+ * @dest: first argument points to char value
+ * @src: second argument points to char value
+ * Return: value of dest
+ */
+char  *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (*src != '\0')
+	{
+		dest[count] = *src;
+		count++;
+		src++;
+	}
+	return (dest);
+}
